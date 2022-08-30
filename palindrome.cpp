2a1b89ba1870308ec5cfdf7644f72044ace9795f
Kshitijgith <kshitij.kamble@vit.edu.in>
2022-08-30 00:32:38 -0700

    second commit

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char str[20]=("");
+int i,k;
+gets(str);
+k=strlen(str);
+cout<<k;
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char str[20]=("");
+int i,k;
+gets(str);
+k=strlen(str);
+cout<<k;
+}
+}
+
